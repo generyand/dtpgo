57122d3ad03facd390b4f6edceca03edd43af8d5
generyand <generyan.dep@gmail.com>
2025-10-02 22:39:23 +0800

    remove padding in main layout login

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <main className="min-h-screen w-full p-6 flex items-center justify-center bg-gradient-to-br from-yellow-50/40 via-white to-amber-50/40 dark:from-gray-900 dark:via-gray-950 dark:to-gray-900">
+    <main className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-yellow-50/40 via-white to-amber-50/40 dark:from-gray-900 dark:via-gray-950 dark:to-gray-900">
       {children}
     </main>
   );
